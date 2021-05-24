0305919c52d5932445d636658ec7aa34ec90bc15
wellitonSouza <wellitonaz@yahoo.com.br>
2021-05-24 11:33:37 -0300

    {Aula 1} criando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="m-auto antialised font-sans font-serif font-mono text-center">
+      <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
+        <img src={logo} className="h-42" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-blue-300"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
